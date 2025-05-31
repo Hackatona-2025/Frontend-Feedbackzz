0f332af88ad56bb34e5164029b25a60e9c0c5433
Tomaz CB <83255611+TomazBettio@users.noreply.github.com>
2025-05-31 08:39:56 -0300

    Ajustes em card de feedbacks

---
+++ src/components/ButtonReport.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+import { Button } from "@/components/ui/button"
+import { Flag } from "lucide-react" // Certifique-se que lucide-react está instalado
+
+export function ButtonReport() {
+  const [clicked, onClick] = useState(false);
+
+  return (
+    <div className="flex flex-wrap items-center gap-2 md:flex-row">
+      <Button
+        className="bg-black hover:bg-black text-white border border-red-500"
+        onClick={() => onClick(!clicked)}
+        >
+    <Flag
+    className={`h-5 w-5 transition-colors duration-200 ${
+      clicked ? "!text-red-500" : "!text-white"
+    }`}
+  />
+</Button>
+
+    </div>
+  )
+}
